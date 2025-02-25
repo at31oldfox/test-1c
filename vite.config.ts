e9278da309a0208bc1d4f5b2396f852fc0f944a2
Тимоновский Александр <atcrew3000@ya.ru>
2025-02-26 00:13:39 +0300

    polyfil

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy';
+
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'IE 11'],
+      polyfills: true,
+    }),
+  ],
+  
 })

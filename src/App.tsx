90ef6243a822501346b2fd40cd12eeade3e27594
Тимоновский Александр <atcrew3000@ya.ru>
2025-02-26 00:38:55 +0300

    css module

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import styles from './App.module.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,7 +23,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code className={styles.red}>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
